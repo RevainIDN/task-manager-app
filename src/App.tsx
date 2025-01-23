037a241abc3a3384accbbe4020688b889eca5726
RevainIDN <kov.igor94@gmail.com>
2025-01-23 19:10:08 +0200

    Добавлена логика изменения световой темы на странице

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './components/Sidebar'
+import TaskBoard from './components/TaskBoard'
 
 export default function App() {
-  const [count, setCount] = useState(0)
+  const [lightTheme, setLightTheme] = useState<boolean>(false);
 
   return (
-    <div className='task-manager'>
-      <div className='sidebar-boards'>
-        <div className='switch-sidebar-cont'>
-          <button className='switch-sidebar'></button>
-        </div>
-        <ul className='boards-list'>
-          <li className='board-item'>
-            <img className='board-img' src="board-logo-01.png" alt="" />
-            <p className='board-text'></p>
-          </li>
-        </ul>
-        <div className='switch-theme'>
-          <button className='theme-btn dark-theme-btn'>Dark</button>
-          <button className='theme-btn light-theme-btn'>Light</button>
-        </div>
-      </div>
-      <div className='task-board'></div>
+    <div className={`task-manager ${lightTheme === true ? 'light-theme' : ''}`}>
+      <Sidebar
+        lightTheme={lightTheme}
+        setLightTheme={setLightTheme}
+      />
+      <TaskBoard />
     </div>
   )
 }
