c83f85bca1d1e841c66b93bcfadbe60b9044dcc4
RevainIDN <kov.igor94@gmail.com>
2025-01-24 19:05:01 +0200

    Реализована кнопка открытия/закрытия бокового меню

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,10 +1,18 @@
 import '../styles/Board.css'
 
-export default function Board() {
+interface BoardProps {
+	closeSidebar: boolean;
+}
+
+export default function Board({ closeSidebar }: BoardProps) {
 	return (
-		<li className='board-item'>
+		<li className={`board-item ${closeSidebar ? 'board-item--close' : ''}`}>
 			<img className='board-img' src="emojis/board-logo-01.png" alt="" />
-			<p className='board-text'>Default Board</p>
+			{closeSidebar ? (
+				null
+			) : (
+				<p className='board-text'>Default Board</p>
+			)}
 		</li>
 	)
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { SetStateAction } from 'react'
+import { useState, SetStateAction } from 'react'
 import '../styles/Sidebar.css'
 import Board from './Board'
 
@@ -8,37 +8,60 @@ interface SidebarProps {
 }
 
 export default function Sidebar({ lightTheme, setLightTheme }: SidebarProps) {
+	const [closeSidebar, setCloseSidebar] = useState<boolean>(false);
+
 	const handleSwitch = () => {
 		setLightTheme(prev => !prev)
 	}
 
+	const handleCloseSidebar = () => {
+		setCloseSidebar(prev => !prev)
+	}
+
 	return (
-		<div className='sidebar-boards'>
+		<div className={`sidebar-boards ${closeSidebar ? 'sidebar-boards--close' : ''}`}>
 			<div className='switch-sidebar-cont'>
-				<button className='switch-sidebar'>
-					<img src={`${lightTheme === false ? 'Close_round-dark_theme.svg' : 'Close_round-light_theme.svg'}`} alt="" />
+				<button className='switch-sidebar' onClick={handleCloseSidebar}>
+					{closeSidebar ? (
+						<img src={`${lightTheme === false ? 'Menu-dark_theme.svg' : 'Menu-light_theme.svg'}`} alt="" />
+					) : (
+						<img src={`${lightTheme === false ? 'Close_round-dark_theme.svg' : 'Close_round-light_theme.svg'}`} alt="" />
+					)}
 
 				</button>
 			</div>
 			<ul className='boards-list'>
-				<Board />
-				<li className='board-add-new'>
+				<Board closeSidebar={closeSidebar} />
+				<Board closeSidebar={closeSidebar} />
+				<li className={`board-add-new ${closeSidebar ? 'change-display--close' : ''}`}>
 					<img src={`${lightTheme === false ? 'Add_round_fill-dark_theme.svg' : 'Add_round_fill-light_theme.svg'}`} alt="" />
 
-					<p className='new-board-text'>Add new board</p>
+					{closeSidebar ? (
+						null
+					) : (
+						<p className='new-board-text'>Add new board</p>
+					)}
 				</li>
 			</ul>
-			<div className='switch-theme' onClick={handleSwitch}>
-				<button className={`theme-btn theme-btn--dark ${lightTheme === false ? 'theme-btn--active' : ''}`}>
-					<img src={`${lightTheme === false ? 'Moon_fill-dark_theme.svg' : 'Moon_fill-light_theme.svg'}`} alt="" />
+			<div className={`${closeSidebar ? 'switch-theme--close' : 'switch-theme'}`} onClick={handleSwitch}>
+				{closeSidebar ? (
+					<button className='theme-btn--active'>
+						<img src={`${lightTheme === false ? 'Moon_fill-dark_theme.svg' : 'Sun_fill-light_theme.svg'}`} alt="" />
+					</button>
+				) : (
+					<>
+						<button className={`theme-btn theme-btn--dark ${lightTheme === false ? 'theme-btn--active' : ''}`}>
+							<img src={`${lightTheme === false ? 'Moon_fill-dark_theme.svg' : 'Moon_fill-light_theme.svg'}`} alt="" />
 
-					Dark
-				</button>
-				<button className={`theme-btn theme-btn--light ${lightTheme === true ? 'theme-btn--active' : ''}`}>
-					<img src={`${lightTheme === false ? 'Sun_fill-dark_theme.svg' : 'Sun_fill-light_theme.svg'}`} alt="" />
+							Dark
+						</button>
+						<button className={`theme-btn theme-btn--light ${lightTheme === true ? 'theme-btn--active' : ''}`}>
+							<img src={`${lightTheme === false ? 'Sun_fill-dark_theme.svg' : 'Sun_fill-light_theme.svg'}`} alt="" />
 
-					Light
-				</button>
+							Light
+						</button>
+					</>
+				)}
 			</div>
 		</div>
 	)
