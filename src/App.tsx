f5b95327e33dd29aa70e26c56b451c34f469db91
RevainIDN <kov.igor94@gmail.com>
2025-01-31 12:52:15 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
   const [boards, setBoards] = useLocalStorage<NewBoardInfo[]>('boards', [{
     id: 1,
     newBoardName: 'Default Board',
-    newBoardLogo: '/task-manager-app/assets/board-logo-01.png',
+    newBoardLogo: 'assets/board-logo-01.png',
     tasks: [{
       id: 1,
       img: '',
